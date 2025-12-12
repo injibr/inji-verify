67a98edf502158723cf42c9f1987b52b748a95d4
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-12 15:26:35 +0530

    [INJIVER-1500] - test VpVerification with presentationDefinitionId (#1046)
    * [INJIVER-1500] - test with presentationDefinitionId
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>
    
    * [INJIVER-1500] - add location origin
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-ui/src/components/Home/VerificationSection/VpVerification.tsx
+++ verify-ui/src/components/Home/VerificationSection/VpVerification.tsx
@@ -131,7 +131,7 @@ const DisplayActiveStep = () => {
                   key={flowType + "_" + activeScreen}
                   triggerElement={ <QrIcon id="OpenID4VPVerification_trigger" className="w-[78px] lg:w-[100px]" aria-disabled={presentationDefinition.input_descriptors.length === 0 } /> }
                   verifyServiceUrl={window.location.origin + window._env_.VERIFY_SERVICE_API_URL}
-                  presentationDefinition={presentationDefinition}
+                  presentationDefinitionId={"201"}
                   onVPProcessed={handleOnVpProcessed}
                   onQrCodeExpired={handleOnQrExpired}
                   onError={handleOnError}
