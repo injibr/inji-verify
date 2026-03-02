7bb5d9beb15f3390ef65295c4a71396322915f96
Abhishek S <127825992+abhishek8shankar@users.noreply.github.com>
2026-03-02 12:44:49 +0530

    [MOSIP-44484] Update values.yaml (#1222)
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>

--- deploy/injiverify-apitestrig/install.sh
+++ deploy/injiverify-apitestrig/install.sh
@@ -19,7 +19,6 @@ function installing_apitestrig() {
   COPY_UTIL=../copy_cm_func.sh
   $COPY_UTIL configmap inji-stack-config default $NS
   $COPY_UTIL configmap keycloak-host keycloak $NS
-  $COPY_UTIL configmap artifactory-share artifactory $NS
   $COPY_UTIL configmap config-server-share config-server $NS
 
   echo echo Copy Secrtes
