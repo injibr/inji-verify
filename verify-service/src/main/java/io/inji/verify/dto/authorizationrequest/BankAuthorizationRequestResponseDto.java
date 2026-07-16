1bb88f018c7df42d233bc13a8ecac418993ab9b4
vinicius.dobbs <vinicius.dobbs@dataprev.gov.br>
2026-07-16 12:56:26 -0300

    fix: removendo responseMode e acceptVPWithoutHolderProof do dto wrapper no fluxo do banco

--- verify-service/src/main/java/io/inji/verify/dto/authorizationrequest/BankAuthorizationRequestResponseDto.java
+++ verify-service/src/main/java/io/inji/verify/dto/authorizationrequest/BankAuthorizationRequestResponseDto.java
@@ -13,24 +13,20 @@ import lombok.Getter;
 public class BankAuthorizationRequestResponseDto {
 
     private final String responseType;
-    private final String responseMode;
     private final long issuedAt;
     private final String clientId;
     private final String presentationDefinitionUri;
     private final VPDefinitionResponseDto presentationDefinition;
     private final String nonce;
     private final String responseUri;
-    private final boolean acceptVPWithoutHolderProof;
 
     public BankAuthorizationRequestResponseDto(AuthorizationRequestResponseDto original, String bankResponseUri) {
         this.responseType = original.getResponseType();
-        this.responseMode = original.getResponseMode();
         this.issuedAt = original.getIssuedAt();
         this.clientId = original.getClientId();
         this.presentationDefinitionUri = original.getPresentationDefinitionUri();
         this.presentationDefinition = original.getPresentationDefinition();
         this.nonce = original.getNonce();
         this.responseUri = bankResponseUri;
-        this.acceptVPWithoutHolderProof = original.isAcceptVPWithoutHolderProof();
     }
 }
